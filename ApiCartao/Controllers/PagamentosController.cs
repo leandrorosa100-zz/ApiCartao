745de9f56c2e349454be60b029bf6c4c256314ea
Leandro Rosa <leandro100@gmail.com>
2020-12-24 11:37:14 -0300

    Adicionar arquivos de projeto.

---
+++ ApiCartao/Controllers/PagamentosController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiCartao.Models;
+
+namespace ApiCartao.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PagamentosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PagamentosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pagamentos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pagamento>>> GetPagamentos()
+        {
+            return await _context.Pagamentos.ToListAsync();
+        }
+
+        // GET: api/Pagamentos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pagamento>> GetPagamento(int id)
+        {
+            var pagamento = await _context.Pagamentos.FindAsync(id);
+
+            if (pagamento == null)
+            {
+                return NotFound();
+            }
+
+            return pagamento;
+        }
+
+        // PUT: api/Pagamentos/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPagamento(int id, Pagamento pagamento)
+        {
+            if (id != pagamento.PagamentoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pagamento).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PagamentoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pagamentos
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Pagamento>> PostPagamento(Pagamento pagamento)
+        {
+            _context.Pagamentos.Add(pagamento);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPagamento", new { id = pagamento.PagamentoId }, pagamento);
+        }
+
+        // DELETE: api/Pagamentos/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Pagamento>> DeletePagamento(int id)
+        {
+            var pagamento = await _context.Pagamentos.FindAsync(id);
+            if (pagamento == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pagamentos.Remove(pagamento);
+            await _context.SaveChangesAsync();
+
+            return pagamento;
+        }
+
+        private bool PagamentoExists(int id)
+        {
+            return _context.Pagamentos.Any(e => e.PagamentoId == id);
+        }
+    }
+}
